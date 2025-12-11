3fad997f00e938e2840b15ac42508ff3cf77cc86
casiansandu <casian.sandu@yahoo.com>
2025-12-11 18:45:32 +0200

    fixed minor stuff

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -78,15 +78,6 @@ export type InventoryItemDto = {
     updatedAt: string
 }
 
-export type InventoryItemDto = {
-    id: string
-    name: string
-    currentStock: number
-    unit: string
-    lowStockThreshold: number
-    updatedAt: string
-}
-
 export type LoyaltyAccount = {
     id: string
     userId: string
